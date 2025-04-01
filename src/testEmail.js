e8391770f5efa568c2a4196788d05c8eabad3669
TMelnychenko <tanyadance30@gmail.com>
2025-04-01 16:49:36 +0300

    test Email

--- src/testEmail.js
+++ src/testEmail.js
@@ -1,21 +1,19 @@
 import nodemailer from "nodemailer";
-import dotenv from "dotenv";
-
-dotenv.config();
+import { getEnvVar } from "./getEnvVar.js"; // Імпортуємо утиліту getEnvVar
 
 const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,  // Переконайся, що тут smtp-relay.brevo.com
-  port: Number(process.env.SMTP_PORT), // 587
+  host: getEnvVar("SMTP_HOST"), // Переконайся, що тут smtp-relay.brevo.com
+  port: Number(getEnvVar("SMTP_PORT")), // 587
   secure: false, // Для 587 має бути false
   auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASSWORD,
+    user: getEnvVar("SMTP_USER"),
+    pass: getEnvVar("SMTP_PASSWORD"),
   },
 });
 
 const mailOptions = {
-  from: process.env.SMTP_FROM,
-  to: "тестовий_емейл@gmail.com",
+  from: getEnvVar("SMTP_FROM"),
+  to: "tanyadance30@gmail.com",
   subject: "Test Email",
   text: "This is a test email from Brevo SMTP",
 };
